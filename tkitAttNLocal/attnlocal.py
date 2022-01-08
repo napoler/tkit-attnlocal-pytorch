294ecfa64c9e55276cbea4f8b946e7b10a62e0a2
Terry chan <napoler2008@gmail.com>
2022-01-08 15:20:12 +0800

    dsd

--- tkitAttNLocal/attnlocal.py
+++ tkitAttNLocal/attnlocal.py
@@ -28,22 +28,31 @@ class AttNLocal(nn.Module):
         """
         datas = []
         for it in range(self.maxlen):
-            new = it*[0]+[1]*self.limit+[0]*self.maxlen
-            datas.append(new[:self.maxlen+self.limit])
+            new = it * [0] + [1] * self.limit + [0] * self.maxlen
+            datas.append(new[:self.maxlen + self.limit])
         return datas
 
     def forward(self, x):
         B, L, D = x.size()
         m = self.autoBulidlMaskLimit()
-        mask = torch.Tensor([m]*B)
+        mask = torch.Tensor([m] * B)
         # print(mask.size())
         # torch.where(mask>0,x,mask)
         # 构建填充
         pad = torch.zeros(B, L, self.limit)
-        xplus = torch.cat((x, pad.to(self.device)), dim=-1)
+        xplus = torch.cat((x, pad.to(x.device)), dim=-1)
         active_loss = mask.view(-1) == 1
         # print(xplus.size())
         xplus_out = xplus.view(-1)[active_loss].view(B, L, -1)
         return xplus_out
 
         pass
+
+
+if __name__ == "__main__":
+    print("start test")
+    # 输入维度和长度一样的矩阵
+    a = torch.randn(32, 10, 10)
+    print("a", a)
+    attL = AttNLocal(10, 5)
+    print(attL(a).size())
---
+++ tkitAttNLocal/attnlocalNew.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import torch
+import torch.nn as nn
+
+
+# import pytorch_lightning as pl
+
+class AttNLocal(nn.Module):
+    """
+    自动限制矩阵
+
+    实现斜对角线保留权重，其他的设为-inf
+
+
+    """
+
+    def __init__(self, maxlen=128, limit=20):
+        super(AttNLocal, self).__init__()
+        self.limit = limit
+        self.maxlen = maxlen
+        pass
+
+    def forward(self, x):
+        B, L, D = x.size()
+        mask = torch.ones_like(x).tril(diagonal=-1) + torch.ones_like(x).triu(diagonal=self.limit)  # 下三角矩阵
+        x[mask == 1] = -float("Inf")
+        return x
+
+        pass
+
+
+if __name__ == "__main__":
+    print("start test")
+    # 输入维度和长度一样的矩阵
+    a = torch.randn(5, 10, 16)
+    # print("a", a)
+    attL = AttNLocal(10, 5)
+    out = attL(a)
+    print(a)
+    print(out.argmax(-1))
+    # print()
